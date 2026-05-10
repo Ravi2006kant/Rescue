8a1fe3bbb663c8ac25c4090fcc8c7e0c70428ed4
Ravi <ravi5gangster@gmail.com>
2026-05-10 11:52:26 +0530

    final

--- lib/introScreen.dart
+++ lib/introScreen.dart
@@ -26,7 +26,7 @@ class _IntroscreenState extends State<Introscreen> {
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Center(
-        child: Container(
+        child: SizedBox(
           child: Text("ResQ",style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold,color: Colors.white),),
         ),
       ),
